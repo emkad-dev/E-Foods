8225a768aae93f25e20de92330aab0b4fb90cc91
emkad.dev <emkad567123@gmail.com>
2026-07-09 16:48:42 +0100

    chore(admin-web): add speed insights

--- apps/admin-web/src/main.tsx
+++ apps/admin-web/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from './App';
 import './styles/global.css';
+
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
